adbe21ed84ed25d87e97eed5ad370e5f9b6dcf1e
jwwasylenko <jwwasy@gmail.com>
2024-11-06 16:06:19 -0700

    Updated Buttons

--- Vienna.js
+++ Vienna.js
@@ -1,14 +1,43 @@
 autumn.addEventListener('click', function(){
     document.body.classList.add('autumn');
-    document.body.classList.remove('spring');
     console.log('autumn');
+    document.body.classList.remove('spring');
+    document.body.classList.remove('summer');
+    document.body.classList.remove('winter');
+    document.getElementById("banner").src="Vienna.webp"; 
 })
 
-spring. addEventListener('click', function(){
+winter.addEventListener('click',function () {
+    document.body.classList.add("winter");
+    console.log('winter');
+    document.body.classList.remove('spring');
+    document.body.classList.remove('autumn');
+    document.body.classList.remove('summer');
+    document.getElementById("banner").src="Vienna_Winter.webp";
+})
+
+
+summer.addEventListener('click',function () {
+    document.body.classList.add('summer');
+    console.log('summer');
+    document.body.classList.remove('spring');
+    document.body.classList.remove('autumn');
+    document.body.classList.remove('winter');
+    document.getElementById("banner").src="Vienna_Summertime.webp"
+
+})
+
+spring.addEventListener('click', function(){
     document.body.classList.add('spring');
     console.log('spring');
         document.body.classList.remove('autumn');
+        document.body.classList.remove('winter');
+        document.body.classList.remove('summer');
+        document.getElementById("banner").src="Vienna_Spring.webp"
 })
+
+
+
 
 
 
